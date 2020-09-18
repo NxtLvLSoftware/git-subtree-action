af38762754f75a771b66195409de1708b06cebec
JackNoordhuis <jackjnoordhuis@gmail.com>
2020-09-18 20:43:22 +1000

    entrypoint.sh: don't clone existing downstream repo

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,6 +9,21 @@ ssh-keyscan github.com > /root/.ssh/known_hosts
 echo "${INPUT_DEPLOY_KEY}" >> /root/.ssh/subtree
 chmod 0600 /root/.ssh/subtree
 
+# Generate sha256 of the downstream repo name
+SPLIT_DIR=$(echo -n "${INPUT_REPO}" | sha256sum)
+SPLIT_DIR="${SPLIT_DIR::-3}"
+
+# Get subtree repository into split directory
+git init --bare "${SPLIT_DIR}"
+
+# Create the subtree split branch
+git subtree split --prefix="${INPUT_PATH}" -b split
+
+# Check for force push to remote
+if [ "$INPUT_FORCE" == "true" ]; then
+	PUSH_ARGS="-f"
+fi
+
 # Resolve downstream branch.
 # If not set then use the event github ref, if the ref isn't set default to master.
 if [ "$INPUT_BRANCH" == "" ]; then
@@ -17,21 +32,6 @@ if [ "$INPUT_BRANCH" == "" ]; then
 	else
 		INPUT_BRANCH="$GITHUB_REF"
 	fi
-fi
-
-# Generate sha256 of the downstream repo name
-SPLIT_DIR=$(echo -n "${INPUT_REPO}" | sha256sum)
-SPLIT_DIR="${SPLIT_DIR::-3}"
-
-# Get subtree repository into split directory
-git clone -b "$INPUT_BRANCH" --bare subtree:"${INPUT_REPO}" "${SPLIT_DIR}"
-
-# Create the subtree split branch
-git subtree split --prefix="${INPUT_PATH}" -b split
-
-# Check for force push to remote
-if [ "$INPUT_FORCE" == "true" ]; then
-	PUSH_ARGS="-f"
 fi
 
 # Push to the subtree directory
